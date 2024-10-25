55b5eb9ca8a5c2ef490508ffb10ea6c0fe5aa940
PixelEdition <pixel.pxed@gmail.com>
2024-10-25 16:04:59 +0700

    update: sequence update

--- src/app/data/sequence/02.js
+++ src/app/data/sequence/02.js
@@ -47,7 +47,7 @@ export const sequence = [
       "text": "น้องคลาว: ณ ที่แห่งนี้ กษัตริย์อียิปต์ผู้ยิ่งใหญ่ถูกจองจำอยู่ในพีระมิดอันลึกลับ แม้ร่างกายของพระองค์จะสิ้นลมหายใจไปแล้ว แต่พระปรีชาญาณยังคงหลงเหลืออยู่"
     },
     "stickman": {
-      "show": false,
+      "show": true,
       "type": "explaining",
       "action": true
     },
@@ -68,8 +68,92 @@ export const sequence = [
       "text": "ในยามที่พระองค์ยังทรงพระชนม์ อาณาจักรของพระองค์เผชิญวิกฤตการณ์มากมาย ด้วยสติปัญญาอันล้ำเลิศ พระองค์ได้วางแผนแก้ไขปัญหาเหล่านั้นไว้ในรูปของจิตรกรรมฝาผนังลึกลับ แต่บัดนี้ อาณาจักรกำลังเผชิญหน้ากับภัยพิบัติครั้งใหม่"
     },
     "stickman": {
+      "show": true,
+      "type": "talking",
+      "action": true
+    },
+    "buttons": {
+      "show": {
+        "primary": true,
+        "secondary": false
+      },
+      "label": {
+        "primary": "ต่อไป",
+        "secondary": null
+      }
+    }
+  },
+  {
+    "dialog": {
+      "show": true,
+      "text": "น้องคลาว: วิธีแก้ปัญหาเหล่านี้ได้จางหายไปพร้อมกับการสวรรคตของกษัตริย์องค์ก่อน เหลือไว้เพียงภาพจิตรกรรมปริศนาบนผนังที่ไม่มีผู้ใดสามารถไขความลับได้... จนถึงบัดนี้"
+    },
+    "stickman": {
+      "show": true,
+      "type": "confused",
+      "action": true
+    },
+    "buttons": {
+      "show": {
+        "primary": true,
+        "secondary": false
+      },
+      "label": {
+        "primary": "ต่อไป",
+        "secondary": null
+      }
+    }
+  },
+  {
+    "dialog": {
+      "show": true,
+      "text": "ข้อความปรากฏขึ้นกะพริบ --หยิบเทียนเพื่อให้สามารถมองทาง--"
+    },
+    "stickman": {
       "show": false,
       "type": "explaining",
+      "action": true
+    },
+    "buttons": {
+      "show": {
+        "primary": true,
+        "secondary": false
+      },
+      "label": {
+        "primary": "ต่อไป",
+        "secondary": null
+      }
+    }
+  },
+  {
+    "dialog": {
+      "show": true,
+      "text": "ปุ่มดาวน์โหลดปรากฏขึ้น --แตะเพื่อรับข้อมูลและตัวถอดรหัสอักษรอียิปต์โบราณ--"
+    },
+    "stickman": {
+      "show": false,
+      "type": "explaining",
+      "action": true
+    },
+    "buttons": {
+      "show": {
+        "primary": true,
+        "secondary": false
+      },
+      "label": {
+        "primary": "ต่อไป",
+        "secondary": null
+      }
+    }
+  },
+  {
+    "dialog": {
+      "show": true,
+      "text": "น้องคลาว: พวกท่านได้รับกุญแจสำคัญแล้ว! บัดนี้ จงไขความลับที่ซ่อนอยู่บนผนังระหว่างตำแหน่ง [สุ่มเลข] นาฬิกา ถึง [สุ่มเลข] นาฬิกา เพื่อช่วยกอบกู้อาณาจักรอันยิ่งใหญ่"
+    },
+    "stickman": {
+      "show": true,
+      "type": "wave",
       "action": true
     },
     "buttons": {
