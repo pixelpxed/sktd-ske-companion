06e2ff2b924d8cc65998268fc21b27846549e5da
PixelEdition <pixel.pxed@gmail.com>
2024-10-29 10:14:53 +0700

    style: updated sequence 2 dialogue

--- src/app/data/sequence/02.js
+++ src/app/data/sequence/02.js
@@ -2,7 +2,7 @@ export const sequence = [
   {
     "dialog": {
       "show": true,
-      "text": "<pyramid plot>"
+      "text": "ยินดีต้อนรับสู่สุสานพีระมิดแห่งความลับ… ที่ที่กษัตริย์อียิปต์ผู้ยิ่งใหญ่ถูกจองจำ"
     },
     "stickman": {
       "show": false,
@@ -23,7 +23,7 @@ export const sequence = [
   {
     "dialog": {
       "show": true,
-      "text": "upon arrival: เสียงกระซิบน่าขนลุกดังก้องในห้อง 'ยินดีต้อนรับสู่สุสานแห่งความลับ...'"
+      "text": "ตอนนี้อาณาจักรกำลังเผชิญกับปัญหามากมาย อย่างไรก็ตาม กษัตริย์ผู้ยิ่งใหญ่ได้จารึกรหัสซึ่งจะช่วยอาณาจักรของเขาไว้"
     },
     "stickman": {
       "show": false,
@@ -44,7 +44,7 @@ export const sequence = [
   {
     "dialog": {
       "show": true,
-      "text": "น้องคลาว: ณ ที่แห่งนี้ กษัตริย์อียิปต์ผู้ยิ่งใหญ่ถูกจองจำอยู่ในพีระมิดอันลึกลับ แม้ร่างกายของพระองค์จะสิ้นลมหายใจไปแล้ว แต่พระปรีชาญาณยังคงหลงเหลืออยู่"
+      "text": "วิธีแก้ปัญหาเหล่านี้ได้จางหายไปพร้อมกับการสวรรคตของกษัตริย์องค์ก่อน เหลือไว้เพียงภาพจิตรกรรมปริศนาบนผนังที่ไม่มีผู้ใดสามารถไขความลับได้... จนถึงบัดนี้"
     },
     "stickman": {
       "show": true,
@@ -62,109 +62,5 @@ export const sequence = [
       }
     }
   },
-  {
-    "dialog": {
-      "show": true,
-      "text": "ในยามที่พระองค์ยังทรงพระชนม์ อาณาจักรของพระองค์เผชิญวิกฤตการณ์มากมาย ด้วยสติปัญญาอันล้ำเลิศ พระองค์ได้วางแผนแก้ไขปัญหาเหล่านั้นไว้ในรูปของจิตรกรรมฝาผนังลึกลับ แต่บัดนี้ อาณาจักรกำลังเผชิญหน้ากับภัยพิบัติครั้งใหม่"
-    },
-    "stickman": {
-      "show": true,
-      "type": "talking",
-      "action": true
-    },
-    "buttons": {
-      "show": {
-        "primary": true,
-        "secondary": false
-      },
-      "label": {
-        "primary": "ต่อไป",
-        "secondary": null
-      }
-    }
-  },
-  {
-    "dialog": {
-      "show": true,
-      "text": "น้องคลาว: วิธีแก้ปัญหาเหล่านี้ได้จางหายไปพร้อมกับการสวรรคตของกษัตริย์องค์ก่อน เหลือไว้เพียงภาพจิตรกรรมปริศนาบนผนังที่ไม่มีผู้ใดสามารถไขความลับได้... จนถึงบัดนี้"
-    },
-    "stickman": {
-      "show": true,
-      "type": "confused",
-      "action": true
-    },
-    "buttons": {
-      "show": {
-        "primary": true,
-        "secondary": false
-      },
-      "label": {
-        "primary": "ต่อไป",
-        "secondary": null
-      }
-    }
-  },
-  {
-    "dialog": {
-      "show": true,
-      "text": "ข้อความปรากฏขึ้นกะพริบ --หยิบเทียนเพื่อให้สามารถมองทาง--"
-    },
-    "stickman": {
-      "show": false,
-      "type": "explaining",
-      "action": true
-    },
-    "buttons": {
-      "show": {
-        "primary": true,
-        "secondary": false
-      },
-      "label": {
-        "primary": "ต่อไป",
-        "secondary": null
-      }
-    }
-  },
-  {
-    "dialog": {
-      "show": true,
-      "text": "ปุ่มดาวน์โหลดปรากฏขึ้น --แตะเพื่อรับข้อมูลและตัวถอดรหัสอักษรอียิปต์โบราณ--"
-    },
-    "stickman": {
-      "show": false,
-      "type": "explaining",
-      "action": true
-    },
-    "buttons": {
-      "show": {
-        "primary": true,
-        "secondary": false
-      },
-      "label": {
-        "primary": "ต่อไป",
-        "secondary": null
-      }
-    }
-  },
-  {
-    "dialog": {
-      "show": true,
-      "text": "น้องคลาว: พวกท่านได้รับกุญแจสำคัญแล้ว! บัดนี้ จงไขความลับที่ซ่อนอยู่บนผนังระหว่างตำแหน่ง 12 นาฬิกา ถึง 2 นาฬิกา เพื่อช่วยกอบกู้อาณาจักรอันยิ่งใหญ่"
-    },
-    "stickman": {
-      "show": true,
-      "type": "wave",
-      "action": true
-    },
-    "buttons": {
-      "show": {
-        "primary": true,
-        "secondary": false
-      },
-      "label": {
-        "primary": "ต่อไป",
-        "secondary": null
-      }
-    }
-  }
+
 ]
