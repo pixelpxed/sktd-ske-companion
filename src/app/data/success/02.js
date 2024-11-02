f8a4e3177b2dcf8ca885973a9fc754efcc431ac4
PixelEdition <pixel.pxed@gmail.com>
2024-11-02 09:20:58 +0700

    feat: straight-forward puzzle success page

--- src/app/data/success/02.js
+++ src/app/data/success/02.js
@@ -8,51 +8,15 @@ roomThreeRandomizer()
 export const success = [
   {
     "page": (
-      <div key="sequence-01" className="dialogue-container">
-        <p>
-          <span className="text-gradient font-bold text-2xl">สวัสดีครับ !</span>
-        </p>
-        <div className="m-4">
-          <StickmanImage
-            type="confetti"
-          />
-        </div>
-        <p>
-          ยอดเยี่ยม! พวกท่านได้ไขปริศนาสำเร็จแล้ว! อาณาจักรจะรอดพ้นจากหายนะแล้ว!
-        </p>
-      </div>
-    ),
-    "cooldown": false
-  },
-  {
-    "page": (
-      <div key="sequence-02" className="dialogue-container">
-        <p>
-          <span className="text-gradient font-bold text-2xl">สวัสดีครับ !</span>
-        </p>
-        <div className="m-4">
-          <StickmanImage
-            type="idle"
-          />
-        </div>
-        <p>
-          คุณได้รับรางวัลจากเทวีฮิปโปเป็นชิ้นส่วนปริศนา!
-        </p>
-      </div>
-    ),
-    "cooldown": false
-  },
-  {
-    "page": (
       <div key="sequence-04" className="dialogue-container">
-        <p>คุณได้รับของวิเศษ</p>
+        <p>คุณได้รับของวิเศษจากเทวีฮิปโปเป็นชิ้นส่วนปริศนา!</p>
         <p className="text-6xl font-bold animate-fade [animation-delay:500ms] opacity-0 [animation-fill-mode:forwards]">
           {(typeof window !== 'undefined')
             ? validJigsaw[parseInt(localStorage.getItem("sktd-ske2024-room03-randomJigsawIndex"))][1]
             : ''}
         </p>
-        <p>แล้ว!!</p>
-        <p className="pt-4">โปรดเดินไปหา Staff ที่ทางออกของห้องนี้เพื่อดำเนินการต่อ!</p>
+        <hr />
+        <p>โปรดเดินไปหา Staff ที่ทางออกของห้องนี้เพื่อดำเนินการต่อ!</p>
         <p className="pt-4">เมื่อเรียบร้อยแล้วให้ไปห้องที่ 3 ได้เลย!</p>
       </div>
     ),
